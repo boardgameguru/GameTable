2fd83d827eef2f7478dac84cff6841c327bb54e0
boardgameguru <thomas.spencer416@gmail.com>
2018-01-18 16:11:24 -0500

    Adjusted routes.rb to fix the auto generated routes for create and destroy being GET requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     root 'welcome#index'
@@ -12,4 +6,5 @@ Rails.application.routes.draw do
     resource :hosts
     resources :tables
     resource :users
+    resources :sessions, only: %i(new create destroy)
 end
