bd06f9388122a714190d3cb1d58421aa011a3a57
boardgameguru <thomas.spencer416@gmail.com>
2018-01-19 15:42:36 -0500

    Implemented sign up function with error and flash display messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }
+
     has_and_belongs_to_many :tables
 end
