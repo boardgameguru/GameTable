7d63e3f062e865c84e5cb50d42c5b38102b493a1
boardgameguru <thomas.spencer416@gmail.com>
2018-01-19 16:43:59 -0500

    Implemented login function.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,25 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-  end
+    def create
+      user = User.find_by email: session_params[:email]
+
+      if user && user.authenticate(session_params[:password])
+          flash[:notice] = "Successfuly logged in!"
+          redirect_to root_path
+      else
+          flash[:alert] = "Invalid credentials"
+          render :new
+      end
+    end
 
   def destroy
   end
+
+    private
+
+    def session_params
+       params.require(:session).permit(:email, :password)
+    end
+
 end
