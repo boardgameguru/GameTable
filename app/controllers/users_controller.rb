2f6ac0997d642efb7fdaccfa945bea2ca90f29fb
boardgameguru <thomas.spencer416@gmail.com>
2018-01-20 16:45:29 -0500

    Added user#edit and user#update functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,23 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = current_user
+    end
+
+    def update
+
+        @user = current_user
+
+        if @user.update_attributes(user_params)
+            flash[:notice] = "Your information has been updated"
+            redirect_to root_path
+        else
+            flash[:alert] = "Sorry, something went wrong"
+            render :edit
+        end
+    end
+
     private
 
     def user_params
