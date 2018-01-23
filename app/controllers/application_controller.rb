df163b178ee6bffc62a1abe6cc86a85191dea400
boardgameguru <thomas.spencer416@gmail.com>
2018-01-23 16:33:08 -0500

    Added navigation and responsive design for logged in hosts. Created helper method current_host (similar to current_user), to check if a host is logged in and pull information.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     helper_method :current_user
+    helper_method :current_host
 
     def current_user
         session[:user_id] && User.find(session[:user_id])
+    end
+
+    def current_host
+       session[:host_id] && Host.find(session[:host_id])
     end
 end
