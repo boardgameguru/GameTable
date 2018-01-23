d1cdb06976afca3d99a09fabf9f07c9012f62131
boardgameguru <thomas.spencer416@gmail.com>
2018-01-23 14:50:06 -0500

    Implemented account creation for hosts.

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,2 +1,24 @@
 class HostsController < ApplicationController
+
+    def new
+        @host = Host.new
+    end
+
+    def create
+        @host = Host.new(host_params)
+
+        if @host.save
+            flash[:notice] = "Welcome to GameTable, #{@host.name}!"
+            redirect_to root_path
+        else
+            flash[:alert] = "We're sorry, but something went wrong."
+            render :new
+        end
+    end
+
+    private
+
+    def host_params
+        params.require(:host).permit(:name, :address, :phone, :email, :password, :password_confirmation)
+    end
 end
