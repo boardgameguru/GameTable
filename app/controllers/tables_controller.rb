e048690200a6f778d7ac20120153e9a863bccf55
boardgameguru <thomas.spencer416@gmail.com>
2018-01-25 12:57:26 -0500

    Implemented table creation with model validations

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,2 +1,25 @@
 class TablesController < ApplicationController
+
+    def new
+       @table = Table.new
+    end
+
+    def create
+        @table = Table.new(table_params)
+        @table.host_id = current_host.id
+
+        if @table.save
+            flash[:notice] = "Your table has been created!"
+            redirect_to dashboard_path
+        else
+            flash[:alert] = "Sorry, something went wrong"
+            render :new
+        end
+    end
+
+    private
+
+    def table_params
+        params.require(:table).permit(:table_name, :table_date, :table_time, :number_of_seats)
+    end
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,7 @@
 class Table < ApplicationRecord
+    validates :table_name, :table_date, :table_time, :number_of_seats, presence: true
+    validates :number_of_seats, numericality: { greater_than: 0}
+
     belongs_to :host
     has_and_belongs_to_many :users
 end
