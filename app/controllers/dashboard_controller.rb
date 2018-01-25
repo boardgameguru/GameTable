bba666a98a5aada87df86cd029708eb98bf81972
boardgameguru <thomas.spencer416@gmail.com>
2018-01-25 13:53:27 -0500

    Added functionality to dashboard for hosts. Allows hosts to see created tables in a list format.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,10 @@ class DashboardController < ApplicationController
     before_action :require_login
 
     def index
+        if current_user
+
+        elsif current_host
+           @main = Table.where(host_id: current_host.id)
+        end
     end
 end
