530088889e0e1bc9e81320a878a0d01cf4560a4b
boardgameguru <thomas.spencer416@gmail.com>
2018-01-26 11:43:43 -0500

    Added visibility of current table reservations on the user dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,9 @@ class DashboardController < ApplicationController
 
     def index
         if current_user
-
+            @main = User.find(current_user).tables
         elsif current_host
-           @main = Table.where(host_id: current_host.id)
+            @main = Table.where(host_id: current_host.id)
         end
     end
 end
