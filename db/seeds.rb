fa4c8053106c7b86148d5924597edada9d3b6d0b
boardgameguru <thomas.spencer416@gmail.com>
2018-02-02 16:57:53 -0500

    Created seed file for development database. Added 3 users, a host, and a single table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Users
+
+User.create(first_name: 'Jane', last_name: 'Doe', email: 'janedoe@gmail.com', password: 'password')
+
+User.create(first_name: 'John', last_name: 'Doe', email: 'johndoe@gmail.com', password: 'password')
+
+User.create(first_name: 'Thomas', last_name: 'Spencer', email: 'thomas.spencer416@gmail.com', password: 'abcd1234')
+
+# Create Host
+
+Host.create(name: 'Store1', address: '123 Street', phone: '1234567890', email: 'store1@store.com', password: 'storepassword')
+
+# Create Tables
+
+Table.create(host_id: 1, table_name: 'Event1', table_date: '2018-02-07', table_time: '2000-01-01 18:00:00', number_of_seats: 7)
+
