815ded0521cff09dba2dff61bff5fe04a16b86ad
boardgameguru <thomas.spencer416@gmail.com>
2018-02-02 19:27:08 -0500

    Created three tests for User model. First test to ensure all fixtures have been properly implemented. Second test to ensure a blank user cannot be created. Finally, the third is to ensure a user cannot be created with the same email as another user.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test '3 users exist' do
+       assert 3, User.all.size
+    end
+
+    test 'cannot create blank user' do
+        user_test = User.new
+        assert_not user_test.valid?
+    end
+
+    test 'new user cannot have same email' do
+       user_test = User.create(first_name: 'Test', last_name: 'test', email: 'johndoe@gmail.com', password: 'password', password_confirmation: 'password')
+        assert_not user_test.valid?
+    end
 end
