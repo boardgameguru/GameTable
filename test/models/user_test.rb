513634998b649be0ab85e5bada4bd47cc1a87d57
boardgameguru <thomas.spencer416@gmail.com>
2018-02-02 19:42:33 -0500

    Added test to ensure a password cannot contain less than six characters.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,9 @@ class UserTest < ActiveSupport::TestCase
        user_test = User.create(first_name: 'Test', last_name: 'test', email: 'johndoe@gmail.com', password: 'password', password_confirmation: 'password')
         assert_not user_test.valid?
     end
+
+    test 'no passwords less than 6 characters' do
+        user_test = User.create(first_name: 'Test', last_name: 'test', email: 'test@gmail.com', password: 'pass', password_confirmation: 'pass')
+        assert_not user_test.valid?
+    end
 end
