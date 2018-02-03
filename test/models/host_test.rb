1c1a49142a2bec3ea55974324cda1a724abdf844
boardgameguru <thomas.spencer416@gmail.com>
2018-02-02 20:45:59 -0500

    Create tests similar to user tests. Ensure fixtures have been set up properly and ensuring validations are working.

--- test/models/host_test.rb
+++ test/models/host_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class HostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test 'two users exist' do
+       assert 2, Host.all.size
+    end
+
+    test 'cannot create blank host' do
+        test_host = Host.new
+        assert_not test_host.valid?
+    end
+
+    test 'cannot create new host with same email' do
+       test_host = Host.new(name: 'teststore', address: 'test', phone: '1234567890', email: 'store1@store.com', password: 'password')
+        assert_not test_host.valid?
+    end
+
+    test 'no passwords less than 6 characters' do
+       test_host = Host.new(name: 'teststore', address: 'test', phone: '1234567890', email: 'test@store.com', password: 'pass')
+        assert_not test_host.valid?
+    end
 end
