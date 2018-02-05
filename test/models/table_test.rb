2cedba084c8f219b210e3eead88eda17a902bae1
boardgameguru <thomas.spencer416@gmail.com>
2018-02-05 10:35:10 -0500

    Added test for checking table validations (both presence of attributes and that seating could not be a negative number)

--- test/models/table_test.rb
+++ test/models/table_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class TableTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
     test 'three tables exist' do
        assert_equal 3, Table.all.size
+    end
+
+    test 'cannot create blank table' do
+       assert_not Table.new.valid?
+    end
+
+    test 'cannot have negative seating' do
+        test_table = Table.new(host_id: 1, table_name: "Test", table_date: 2018-02-07, table_time: '2018-02-07 18:00:00', number_of_seats: -2)
+        assert_not test_table.valid?
     end
 end
